24e8bcfd4ac25aec61d6acb9994e1f3e84f98d56
Joel Tong <me@joeltong.org>
2015-02-21 00:54:19 +0800

    committing more blocks

--- src/js/blastFramework.js
+++ src/js/blastFramework.js
@@ -1,7 +1,20 @@
 'use strict';
 
+/**
+ * BLAST Framework for AI2
+ *
+ * @author TONG Haowen Joel
+ *
+ */
+
 var blastFramework = (function () {
     /** Private vars **/
+    /**
+     * Hooks will be decorated dynamically as function is called from AI2 Java interface
+     *
+     * @type {{preload: Function, create: Function, update: Function}}
+     * @private
+     */
     var _game = {
         preload: function () {
         },
@@ -11,13 +24,20 @@ var blastFramework = (function () {
         }
     };
 
-    // store vars
+    /**
+     * Accumulator object where variables will be stored.
+     * @type {{game: null}}
+     * @private
+     */
     var _accumulator = {
         game: null
     };
 
-    function sayHello() {
-        console.log("hello world!");
+    /**
+     * Prints the version
+     */
+    function version() {
+        console.log("BLAST v0.0.1");
     }
 
     /******************** Start of AI2 Blocks ********************/
@@ -61,6 +81,14 @@ var blastFramework = (function () {
 
     }
 
+    function initGroup(jsonData) {
+        var target = "_accumulator.game"
+            , id = jsonData.id || null;
+
+        var code = '_accumulator[\"' + id + '\"]' + '=' + target + '.add.group();';
+        return code;
+
+    }
 
 
     /******************** End of AI2 Blocks ********************/
@@ -82,12 +110,19 @@ var blastFramework = (function () {
             return loadResource(jsonData);
         } else if (blockName == 'AddSprite') {
             return addSprite(jsonData);
+        } else if (blockName == 'InitGroup') {
+            return initGroup(jsonData);
         }
 
         /** No blocks **/
         return '';
     }
 
+    /**
+     * Function used to generate code blocks and insert in right function.  Uses eval (!!)
+     *
+     * @param jsonData The jsonData should contain both handler and block information.
+     */
     function appendCode(jsonData) {
         var oldFunc = _game[jsonData.handler]; // get the target function to modify
         _game[jsonData.handler] = function () {
@@ -101,7 +136,7 @@ var blastFramework = (function () {
     }
 
     return {
-        sayHello: sayHello,
+        version: version,
         appendCode: appendCode,
         createGame: createGame
     }
@@ -109,7 +144,8 @@ var blastFramework = (function () {
 
 })();
 
-console.log(blastFramework.sayHello());
+/** Test blocks **/
+
 blastFramework.appendCode({
     id: 'sky',
     handler: 'preload',
@@ -125,11 +161,24 @@ blastFramework.appendCode({
     filepath : 'img/assets/dude.png'
 });
 blastFramework.appendCode({
+    id: 'ground',
+    handler: 'preload',
+    block: 'LoadResource',
+    loaderType: 'image',
+    filepath : 'img/assets/platform.png'
+});
+blastFramework.appendCode({
     resource: 'sky',
     handler: 'create',
     block: 'AddSprite',
     x: 0,
     y: 0
+});
+//TODO: implement
+blastFramework.appendCode({
+    id: 'platforms',
+    handler: 'create',
+    block: 'InitGroup'
 });
 blastFramework.appendCode({
     handler: 'create',
