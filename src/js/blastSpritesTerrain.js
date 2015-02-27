239d04fee9c4d513f611a58b4d7224f81a6685dd
Joel Tong <me@joeltong.org>
2015-02-27 16:55:09 +0800

    moved init and kill routines to internal ones.

--- src/js/blastSpritesTerrain.js
+++ src/js/blastSpritesTerrain.js
@@ -4,26 +4,47 @@
 
 'use strict';
 
-var Blast = Blast || {};
-var __generators = Blast.prototype.sprite.generators;
+var Blast = Blast || {}; // blast library
+var $blast = $blast || new Blast(); // instance of blast
+var __generators = Blast.prototype.sprite.generators; // extending sprite generators
 
 
 __generators.SimpleSprite = function(name) {
     var _scope = undefined;
 
-    var kill = function() {
+    var _kill = function() {
         console.debug('Kill routine for name=' + name + ' called.');
         delete this;
     }
 
-    var init = function() {
-        _scope = this;
+    var _init = function(scope) {
+        _scope = scope;
         _scope.name = name;
         $blast.registerObject(name, _scope);
         console.debug('SimpleSprite=' + name + ' created');
     }
 
+    /**
+     * Override this implementation.
+     */
+    var init = function() {
+        /** Insert code here **/
+        /** End insert code here **/
+        _init(this);
+    }
+
+    /**
+     * Override this implementation.
+     */
+    var kill = function() {
+        /** Insert code here **/
+        /** End insert code here **/
+        _kill();
+    }
+
     return {
+        _init: _init,
+        _kill: _kill,
         init: init,
         kill: kill
     }
